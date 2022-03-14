4c008424ea477a70a409b05c02083ffaaa177d28
javiermesonero <93916105+javiermesonero@users.noreply.github.com>
2022-03-14 09:46:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ def visualizeME_and_describe_violinbox(dataframe, categ_var, numeric_var, palett
     
 
     #plt.show()
-    st.write(table)
+    st.dataframe(table)
 
 def better_visualizeME_and_describe_violinbox(dataframe, categ_var, numeric_var, categ_var2= None, palette='tab10'):
     '''
