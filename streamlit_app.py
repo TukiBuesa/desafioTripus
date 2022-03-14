fef158076c349b88942a8b12ace118e2e48742fa
javiermesonero <93916105+javiermesonero@users.noreply.github.com>
2022-03-14 09:12:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,13 +66,15 @@ def visualizeME_and_describe_violinbox(dataframe, categ_var, numeric_var, palett
     '''
     # Generate ViolinBOX graph
     num_cat = len(list(dataframe[categ_var].unique()))
-    ax=plt.figure(figsize=(num_cat*1.5,10))
-    ax=sns.violinplot(x=categ_var, y=numeric_var, data=dataframe, palette= palette)
+    fig,ax = plt.figure(figsize=(num_cat*1.5,10))
+    ax = sns.violinplot(x=categ_var, y=numeric_var, data=dataframe, palette= palette)
     ax = sns.boxplot(x=categ_var, y=numeric_var, data=dataframe,fliersize=0, color='white')
     ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha='right')
     titulo= numeric_var.upper() + '_vs_' + categ_var.upper()
     plt.title(titulo, fontsize=15)
-    st.pyplot(ax)
+
+    st.pyplot(fig)
+
     # Save graph
     if save == True:
         graph = 'visualizeME_Graphic_violinbox_' + titulo.lower() + '.png'
